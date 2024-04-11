b22a64ac424434a2743ae5a1bb1831d5750a0c6f
marcus <marcus.4vu@gmail.com>
2024-04-11 10:45:12 +0700

    ex4.2

---
+++ Sophomore/Semester_2/Operating_System/lab2/2212828-Lab2/prob2/sum_multi-threads.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+typedef struct
+{
+  long long int start;
+  long long int end;
+  long long int sum;
+} Threadform;
+
+void *calcu(void *arg)
+{
+  Threadform *args = (Threadform *)arg;
+  args->sum = 0;
+  for (long long int i = args->start; i <= args->end; i++)
+  {
+    args->sum += i;
+  }
+  return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc != 3)
+  {
+    printf("ERROR: inputs mustbe ./sum_multi-thread threads n\n");
+    return 1;
+  }
+  long long int n = atoll(argv[2]);
+  int numThreads = atoi(argv[1]);
+  clock_t start = clock();
+  pthread_t threads[numThreads];
+  Threadform threadform[numThreads];
+  long long int eachThreadCal = n / numThreads;
+  for (int i = 0; i < numThreads; i++)
+  {
+    threadform[i].start = i * eachThreadCal + 1;
+    threadform[i].end = (i == numThreads - 1) ? n : (i + 1) * eachThreadCal;
+    pthread_create(&threads[i], NULL, calcu, (void *)&threadform[i]);
+  }
+  long long int sum = 0;
+  for (int i = 0; i < numThreads; i++)
+  {
+    pthread_join(threads[i], NULL);
+    sum += threadform[i].sum;
+  }
+  clock_t end = clock();
+  double time = (double)(end - start) / CLOCKS_PER_SEC;
+  printf("sum_multi-threads = %lld, Runtime = %f\n", sum, time);
+}
--- Sophomore/Semester_2/Operating_System/lab2/2212828-Lab2/prob2/sum_serial.c
+++ Sophomore/Semester_2/Operating_System/lab2/2212828-Lab2/prob2/sum_serial.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
-
-int main()
+#include <stdlib.h>
+#include <time.h>
+long long int calcu(long long int n)
 {
-  int sum = 0;
-  printf("Enter a number: ");
-  int n = scanf("%d", &n);
-  for (int i = 1; i <= n; i++)
+  long long int sum = 0;
+  for (long long int i = 1; i <= n; i++)
   {
     sum += i;
   }
-  printf("sum_serial = %d\n", sum);
+  return sum;
+}
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+    printf("ERROR: inputs mustbe ./sum_serial n\n");
+    return 1;
+  }
+  long long int n = atoll(argv[1]);
+  clock_t start = clock();
+  long long int sum = calcu(n);
+  clock_t end = clock();
+  double time = (double)(end - start) / CLOCKS_PER_SEC;
+  printf("sum_serial = %lld, Runtime = %f\n", sum, time);
 }
