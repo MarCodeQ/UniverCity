d46a08eee8cfd043dd6576bc3747f45f9514fea7
marcus <marcus.4vu@gmail.com>
2024-04-15 12:27:06 +0700

    AdvancedProgramming-lab3-part1

--- Sophomore/Semester_2/Advanced_Programming/Lab3/A3_VuMinhQuan_2212828/part1.cpp
+++ Sophomore/Semester_2/Advanced_Programming/Lab3/A3_VuMinhQuan_2212828/part1.cpp
@@ -1,2 +1,264 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib> // for rand()
+#include <ctime>   // for time()
+
 using namespace std;
+
+class Student
+{
+public:
+  Student(const string &name, const string &dob, const string &school, const string &course)
+      : name(name), dob(dob), school(school), course(course) {}
+
+  virtual ~Student() {}
+
+  virtual void displayDetails() const
+  {
+    cout << "Name: " << name << ", Date of Birth: " << dob
+         << ", School: " << school << ", Course: " << course;
+  }
+
+  virtual void DoAssignment() = 0;
+  virtual void TakeTest() = 0;
+  virtual void TakeExam() = 0;
+
+protected:
+  string name;
+  string dob;
+  string school;
+  string course;
+};
+
+class UniStudent : public Student
+{
+public:
+  UniStudent(const string &name, const string &dob, const string &school, const string &course, int semesters)
+      : Student(name, dob, school, course), semesters(semesters) {}
+
+  void displayDetails() const override
+  {
+    Student::displayDetails();
+    cout << ", Semesters: " << semesters << endl;
+  }
+
+  void DoAssignment() override
+  {
+    cout << "Enter scores for UniStudent assignments:" << endl;
+    for (int i = 1; i <= semesters * 4; ++i)
+    {
+      int score;
+      cout << "Assignment " << i << " Score: ";
+      cin >> score;
+      assignmentScores.push_back(score);
+    }
+  }
+
+  void TakeTest() override
+  {
+    cout << "Enter scores for UniStudent tests:" << endl;
+    for (int i = 1; i <= semesters * 4; ++i)
+    {
+      int score;
+      cout << "Test " << i << " Score: ";
+      cin >> score;
+      testScores.push_back(score);
+    }
+  }
+
+  void TakeExam() override
+  {
+    cout << "Enter scores for UniStudent exams:" << endl;
+    for (int i = 1; i <= semesters; ++i)
+    {
+      int score;
+      cout << "Exam Semester " << i << " Score: ";
+      cin >> score;
+      examScores.push_back(score);
+    }
+  }
+
+private:
+  int semesters;
+  vector<int> assignmentScores;
+  vector<int> testScores;
+  vector<int> examScores;
+};
+
+class CollegeStudent : public Student
+{
+public:
+  CollegeStudent(const string &name, const string &dob, const string &school, const string &course, int semesters)
+      : Student(name, dob, school, course), semesters(semesters) {}
+
+  void displayDetails() const override
+  {
+    Student::displayDetails();
+    cout << ", Semesters: " << semesters << endl;
+  }
+
+  void DoAssignment() override
+  {
+    cout << "Enter scores for CollegeStudent assignments:" << endl;
+    for (int i = 1; i <= semesters * 3; ++i)
+    {
+      int score;
+      cout << "Assignment " << i << " Score: ";
+      cin >> score;
+      assignmentScores.push_back(score);
+    }
+  }
+
+  void TakeTest() override
+  {
+    cout << "Enter scores for CollegeStudent tests:" << endl;
+    for (int i = 1; i <= semesters * 3; ++i)
+    {
+      int score;
+      cout << "Test " << i << " Score: ";
+      cin >> score;
+      testScores.push_back(score);
+    }
+  }
+
+  void TakeExam() override
+  {
+    cout << "Enter scores for CollegeStudent exams:" << endl;
+    for (int i = 1; i <= semesters; ++i)
+    {
+      int score;
+      cout << "Exam Semester " << i << " Score: ";
+      cin >> score;
+      examScores.push_back(score);
+    }
+  }
+
+private:
+  int semesters;
+  vector<int> assignmentScores;
+  vector<int> testScores;
+  vector<int> examScores;
+};
+
+class University
+{
+public:
+  University(const string &name) : name(name) {}
+
+  void addUniStudent(const string &name, const string &dob, const string &school, const string &course, int semesters)
+  {
+    uniStudents.push_back(new UniStudent(name, dob, school, course, semesters)); // Using pointers
+  }
+
+  void addCollegeStudent(const string &name, const string &dob, const string &school, const string &course, int semesters)
+  {
+    collegeStudents.push_back(new CollegeStudent(name, dob, school, course, semesters)); // Using pointers
+  }
+
+  void displayAllStudents() const
+  {
+    cout << "University Name: " << name << endl;
+    cout << "University Students:\n";
+    for (const auto &student : uniStudents)
+    {
+      student->displayDetails();
+      student->DoAssignment();
+      student->TakeTest();
+      student->TakeExam();
+      cout << endl;
+    }
+    cout << "\nCollege Students:\n";
+    for (const auto &student : collegeStudents)
+    {
+      student->displayDetails();
+      student->DoAssignment();
+      student->TakeTest();
+      student->TakeExam();
+      cout << endl;
+    }
+  }
+
+  ~University()
+  {
+    for (auto student : uniStudents)
+    {
+      delete student;
+    }
+    for (auto student : collegeStudents)
+    {
+      delete student;
+    }
+  }
+
+private:
+  string name;
+  vector<UniStudent *> uniStudents;         // Using pointers
+  vector<CollegeStudent *> collegeStudents; // Using pointers
+};
+
+int readChoice()
+{
+  int choice;
+  cout << "\nStudent Management System\n";
+  cout << "1. Add UniStudent\n";
+  cout << "2. Add CollegeStudent\n";
+  cout << "3. Display Students\n";
+  cout << "0. Exit\n";
+  cout << "Enter your choice: ";
+  cin >> choice;
+  return choice;
+}
+
+int main()
+{
+  srand(time(nullptr));
+
+  University university("My University");
+  int choice = 0;
+  string name, dob, school, course;
+  int semesters;
+  do
+  {
+    choice = readChoice();
+    switch (choice)
+    {
+    case 1:
+      cout << "Enter UniStudent Name: ";
+      cin >> name;
+      cout << "Enter UniStudent Date of Birth (YYYY-MM-DD): ";
+      cin >> dob;
+      cout << "Enter UniStudent School: ";
+      cin >> school;
+      cout << "Enter UniStudent Course: ";
+      cin >> course;
+      cout << "Enter UniStudent Semesters: ";
+      cin >> semesters;
+      university.addUniStudent(name, dob, school, course, semesters);
+      break;
+    case 2:
+      cout << "Enter CollegeStudent Name: ";
+      cin >> name;
+      cout << "Enter CollegeStudent Date of Birth (YYYY-MM-DD): ";
+      cin >> dob;
+      cout << "Enter CollegeStudent School: ";
+      cin >> school;
+      cout << "Enter CollegeStudent Course: ";
+      cin >> course;
+      cout << "Enter CollegeStudent Semesters: ";
+      cin >> semesters;
+      university.addCollegeStudent(name, dob, school, course, semesters);
+      break;
+    case 3:
+      university.displayAllStudents();
+      break;
+    case 0:
+      cout << "Exiting program.\n";
+      break;
+    default:
+      cout << "Invalid choice. Please try again." << endl;
+    }
+  } while (choice != 0);
+
+  return 0;
+}
