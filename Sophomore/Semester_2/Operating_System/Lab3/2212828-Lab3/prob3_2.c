552dcb28b58edc97b82c3ed89e236d22a4ee2d0c
marcus <marcus.4vu@gmail.com>
2024-04-26 17:33:57 +0700

    os|lab3|prob3_2-finish

--- Sophomore/Semester_2/Operating_System/Lab3/2212828-Lab3/prob3_2.c
+++ Sophomore/Semester_2/Operating_System/Lab3/2212828-Lab3/prob3_2.c
@@ -6,12 +6,11 @@
 #define BUF_SIZE 2
 #define THREADS 1          // 1 producer and 1 consumer
 #define LOOPS 3 * BUF_SIZE // variable
+sem_t empty, full;
 // Initiate shared buffer
 int buffer[BUF_SIZE];
 int fill = 0;
 int use = 0;
-sem_t sem;
-/*TODO: Fill in the synchronization stuff */
 
 void put(int value); // put data into buffer
 int get();           // get data from buffer
@@ -22,12 +21,11 @@ void *producer(void *arg)
   int tid = *((int *)arg);
   for (i = 0; i < LOOPS; i++)
   {
-    /*TODO: Fill in the synchronization stuff */
+    sem_wait(&empty);
     put(i); // line P2
     printf("Producer %d put data %d\n", tid, i);
-    sem_post(&sem);
+    sem_post(&full);
     sleep(1);
-    /*TODO: Fill in the synchronization stuff */
   }
   pthread_exit(NULL);
 }
@@ -36,16 +34,18 @@ void *consumer(void *arg)
 {
   int i, tmp = 0;
   int tid = *((int *)arg);
-  sem_wait(&sem);
-  while (tmp != -1)
+  while (1)
   {
-    /*TODO: Fill in the synchronization stuff */
+    sem_wait(&full);
     tmp = get(); // line C2
+    if (tmp == -1)
+    {
+      sem_post(&full); // Release the semaphore before exiting
+      break;
+    }
     printf("Consumer %d get data %d\n", tid, tmp);
     sleep(1);
-    if (tmp == 5)
-      break;
-    /*TODO: Fill in the synchronization stuff */
+    sem_post(&empty);
   }
   pthread_exit(NULL);
 }
@@ -57,7 +57,8 @@ int main(int argc, char **argv)
   pthread_t producers[THREADS];
   pthread_t consumers[THREADS];
 
-  /*TODO: Fill in the synchronization stuff */
+  sem_init(&empty, 0, BUF_SIZE);
+  sem_init(&full, 0, 0);
 
   for (i = 0; i < THREADS; i++)
   {
@@ -75,7 +76,8 @@ int main(int argc, char **argv)
     pthread_join(consumers[i], NULL);
   }
 
-  /*TODO: Fill in the synchronization stuff destroy (if needed) */
+  sem_destroy(&empty);
+  sem_destroy(&full);
 
   return 0;
 }
@@ -91,5 +93,7 @@ int get()
   int tmp = buffer[use];      // line g1
   buffer[use] = -1;           // clean the item
   use = (use + 1) % BUF_SIZE; // line g2
+  if (tmp == LOOPS - 1)
+    return -1; // Indicate end of data for consumer
   return tmp;
 }
